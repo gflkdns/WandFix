b2ad9aea7f6aacaf1e54567559dcd6175fc7e0e8
miqt <miqingtang@163.com>
2018-12-22 15:17:06 +0800

    APT动态生成热修复打包脚本

--- app/src/main/java/com/example/miqt/dexmvppdemo/MainActivity.java
+++ app/src/main/java/com/example/miqt/dexmvppdemo/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Wand.with(this).init("hotfix_pack.dex");
+        Wand.with(this).init();
         ClassInstall.inject(this);
     }
 
