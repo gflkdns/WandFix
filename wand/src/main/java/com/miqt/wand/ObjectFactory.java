612ec9102a223daee935eb5d225a516f64fc7451
miqt <miqingtang@163.com>
2019-01-17 16:33:31 +0800

    private 构造方法

--- wand/src/main/java/com/miqt/wand/ObjectFactory.java
+++ wand/src/main/java/com/miqt/wand/ObjectFactory.java
@@ -2,9 +2,47 @@ package com.miqt.wand;
 
 import com.miqt.wand.anno.ParentalEntrustmentLevel;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class ObjectFactory {
+
+    public static <T> T invokeStaticMethod(String classname, String methodName,
+                                           ParentalEntrustmentLevel level, Object... pram) {
+        return invokeMethod(null, classname, methodName, level, pram);
+    }
+
+    public static <T> T invokeMethod(Object object, String classname, String methodName,
+                                     ParentalEntrustmentLevel level, Object... pram) {
+        try {
+            Class<T> ap = (Class<T>) Wand.get().loadClass(classname, level);
+            T o = null;
+            if (pram.length == 0) {
+                Method method = ap.getDeclaredMethod(methodName);
+                method.setAccessible(true);
+                o = (T) method.invoke(object);
+            } else {
+                Class[] aClass = new Class[pram.length];
+                for (int i = 0; i < aClass.length; i++) {
+                    aClass[i] = pram[i].getClass();
+                }
+                Method method = ap.getDeclaredMethod(methodName, aClass);
+                method.setAccessible(true);
+                o = (T) method.invoke(object, pram);
+            }
+            return o;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     public static <T> T make(String classname, Object... pram) {
         return make(classname, ParentalEntrustmentLevel.NEVER);
@@ -12,17 +50,20 @@ public class ObjectFactory {
 
     public static <T> T make(String classname, ParentalEntrustmentLevel level, Object... pram) {
         try {
-            Class<T> ap = (Class<T>) Wand.get()
-                    .loadClass(classname, level);
+            Class<T> ap = (Class<T>) Wand.get().loadClass(classname, level);
             T o = null;
             if (pram.length == 0) {
-                o = ap.newInstance();
+                Constructor<T> constructor = ap.getDeclaredConstructor();
+                constructor.setAccessible(true);
+                o = constructor.newInstance();
             } else {
                 Class[] aClass = new Class[pram.length];
                 for (int i = 0; i < aClass.length; i++) {
                     aClass[i] = pram[i].getClass();
                 }
-                o = ap.getConstructor(aClass).newInstance(pram);
+                Constructor<T> constructor = ap.getDeclaredConstructor(aClass);
+                constructor.setAccessible(true);
+                o = constructor.newInstance(pram);
             }
             return o;
         } catch (ClassNotFoundException e) {
