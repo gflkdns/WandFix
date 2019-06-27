ff0f380143cec740914abeb24cc801dbd53cfa30
miqt <miqingtang@163.com>
2019-06-27 17:13:37 +0800

    一个类A单例，在热修复包中B对象跟不在热修复包中的C对象请求单例实例的时候，这个时候是有两个实例的，因为他们在不同的classloader

--- app/src/main/java/com/miqt/demo/teststatic/GetString.java
+++ app/src/main/java/com/miqt/demo/teststatic/GetString.java
@@ -4,12 +4,12 @@ import com.miqt.wand.anno.AddToFixPatch;
 
 @AddToFixPatch
 public class GetString {
-    public static final String ccc = "ccc2";
-    public static String bbb = "bbb2";
+    public static final String ccc = "ccc";
+    public static String bbb = "bbb";
 
     public static String getString() {
-        return "aaa2";
+        return "aaa";
     }
 
-    public String ddd = "ddd2";
+    public String ddd = "ddd";
 }
